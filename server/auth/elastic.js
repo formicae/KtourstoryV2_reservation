35a2433edeedbe483f02c60865a47f2e27e13c36
formicae <charly0102@naver.com>
2020-02-01 17:31:25 +0900

    2020.02.01 : elastic host dif by process.env

--- server/auth/elastic.js
+++ server/auth/elastic.js
@@ -1,7 +1,12 @@
 const elastic = require('elasticsearch');
 const log = require('../../log');
-let client = new elastic.Client({ host:'35.190.50.17'}); // for local-test (load-balancer)
-// for virtualmachine in GCP : host = 10.0.0.108:9200
+const nodeEnv = process.env.NODE_ENV;
+let client;
+if (nodeEnv === 'PRODUCTION') {
+    client = new elastic.Client({ host:'10.0.0.108:9200'}); // for local-test (load-balancer)
+} else {
+    client = new elastic.Client({ host:'35.190.50.17'}); // for local-test (load-balancer)
+}
 client.ping({ requestTimeout: 20000}, (err) => {
     if (err) {
         log.error('Elastic', 'Elasticserach', 'elastic server is down');
